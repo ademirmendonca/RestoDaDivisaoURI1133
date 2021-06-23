6d274cf4c767a4d4d2016cce566eda94ad0e468c
Ademir Mendonça <ademir_mendonca@hotmail.com>
2021-06-23 13:06:42 -0300

    Add project files.

---
+++ RestoDaDivisaoURI1133/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RestoDaDivisaoURI1133
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+                        
+            int menor = x;
+            int maior = y;
+
+            if (x > y)
+            {
+                menor = y;
+                maior = x;
+            }
+
+            
+            for (int i = menor+1; i < maior; i++)
+            {                
+                if (i % 5==2 || i % 5==3)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
